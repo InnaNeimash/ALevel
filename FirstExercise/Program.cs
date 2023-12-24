0dd6101f762a065edb18e6aacc15870f3e678d56
InnaNeimash <anni_mail@ukr.net>
2023-12-24 17:36:41 +0300

    Exercise 1

---
+++ FirstExercise/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace FirstExercise
+{
+    using System;
+        internal class Program
+    {
+                        static void Main()
+        {
+            Random random = new Random();
+           Console.WriteLine("Enter the Number of Elemrents in the Array");//запит кількостыі елементів в масиві
+           int N= Console.Read();
+           int[] A = new int[N];
+           for (int i = 0; i < N; i++)
+           { A[i] = random.Next(-200,200);
+           }
+            Console.WriteLine (A[1]);
+            Console.WriteLine(A[2]);
+            int k=0;
+            for (int i = 0; i < N; i++)
+            {
+                if (A[i]>=-100 && A[i]<=100)
+                { 
+                    k = k + 1;
+               }
+            }
+           Console.WriteLine($"Number of elements,{k}");
+        }
+    }
+}
