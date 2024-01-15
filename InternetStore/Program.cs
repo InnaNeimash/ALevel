0e8b08f774b042e5492687127be597dae7e16284
InnaNeimash <anni_mail@ukr.net>
2024-01-15 22:55:26 +0300

    Internet Store

---
+++ InternetStore/Program.cs
@@ -0,0 +1,58 @@
+﻿using Models;
+using Services;
+using System;
+namespace Models
+{
+    public class Program
+    {
+        private static readonly object FirstName;
+        private static readonly object LastName;
+
+        static bool IsYes(string answer)
+        {
+            return answer.ToLower() == "y";
+        }
+
+        static void Main(string[] args)
+        {
+            ProductService productService = new ProductService();
+            OrderService orderService = new OrderService();
+            Console.WriteLine("Welcome to the Groceries internet store");
+            Console.WriteLine("Please, select an action");
+            Console.WriteLine("1 - I want to order product");
+            Console.WriteLine("2 - No, thanks");
+            int NumberAction = Convert.ToInt32(Console.ReadLine());
+            switch (NumberAction)
+            {
+                case 1: productService.ShowCatalog(); break;
+                case 2: break;
+            }
+            bool yes = false;
+            do
+            {
+                Console.WriteLine("Do you want to add products in the basket? yes/no");
+                yes = IsYes(Console.ReadLine());
+                if (yes)
+                {
+                    productService.ShowCatalog();
+                    Console.WriteLine("Enter the product number you want to add");
+                    int numberproduct = Convert.ToInt32(Console.ReadLine());
+                    productService.AddToBasket(numberproduct);
+                }
+            } while (yes);
+                Console.WriteLine("Yor basket");
+                productService.ShowBasket();
+             Console.WriteLine("Do you want to order? yes/no");
+                yes=IsYes(Console.ReadLine());
+                if (yes) 
+                {
+                  Console.WriteLine($"Please, enter your First and Last name {FirstName} {LastName}");
+                  Console.ReadLine();
+                  Console.WriteLine("Yor order has been successfully completed");
+                  Console.WriteLine(DateTime.Now);
+                  Console.WriteLine("Your order number 256");
+                  orderService.CreateOrder();
+                }
+        }
+    }
+} 
---
+++ InternetStore/Services/ProductService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using Models;
+
+namespace Services
+{
+    public class ProductService
+    {
+        public decimal TotalSum;
+        public List<Product> Products;
+        public List<Product> Nbasket;
+        public ProductService()
+        {
+            Products = new List<Product>
+            {
+                new ("Bread", 25, 1),
+                new ("Milk", 20, 1),
+                new ("Cheese", 260, 1),
+                new ("Whater", 20, 1),
+                new ("Juice", 55, 1),
+                new ("Meat", 425, 1),
+                new ("Sausages", 21, 1),
+                new ("10 Eegs", 65, 1),
+                new ("Sour Cream", 55, 1),
+                new ("Butter", 65, 1),
+                new ("Sunflower oil", 45, 1),
+                new ("Olive oil", 285, 1),
+                new ("Tomato paste", 10, 1),
+                new ("Fish", 450, 1),
+                new ("Ham", 650, 1)
+            };
+            Nbasket = new List<Product>();
+        }
+        public void AddToBasket(int numberproduct)
+        {
+            Nbasket.Add(Products[numberproduct - 1]);
+            Console.WriteLine($"Product{Products[numberproduct - 1].NameProduct} successfully added to Basket");
+            Console.WriteLine($"There are {Nbasket.Count} products");
+        }
+        public void ShowCatalog()
+        {
+            Console.WriteLine("Products catalog");
+            Console.WriteLine("Name products,Price,Quantity");
+            int number = 1;
+            foreach (Product product in Products)
+            {
+                Console.Write(number + ". ");
+                product.Print();
+                number++;
+            }
+        }
+        public void ShowBasket()
+        {
+            Console.WriteLine("Products basket");
+            int number = 1;
+            foreach (Product product in Nbasket)
+            {
+                Console.Write(number + ". ");
+                product.Print();
+                TotalSum += product.Price;
+                number++;
+            }
+            Console.WriteLine($"Total price {TotalSum} UAH");
+        }
+    }
+}
