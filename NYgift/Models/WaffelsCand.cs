39b456e24fb9685b2680b474abb1a37fb11ab32b
InnaNeimash <anni_mail@ukr.net>
2024-01-25 17:54:56 +0300

    Homework 7

---
+++ NYgift/Models/WaffelsCand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NYgift.Models
+{
+    public class WaffelsCand
+    {
+        public int Weight = 20;
+    }
+}
